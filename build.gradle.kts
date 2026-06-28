dabe7f79da1da070d865d849fbbbf70876648126
Mikhail Gubanov <gubanov.mihail7@wb.ru>
2026-06-28 13:21:52 +0200

    test: add parametrized tests for applyMarkup (4 cases)

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,13 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.0")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+kotlin {
+    jvmToolchain(21)
 }
 
 application {
